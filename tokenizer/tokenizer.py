ef2fb5677afd19fa8eaa514a69c8823d224b73bd
Adeoluwa Ayangade <adeoluwa.ayangade@leovegas.com>
2026-08-04 18:51:59 +0100

    Adds tokenizer implementation using toktoken

---
+++ tokenizer/tokenizer.py
@@ -0,0 +1,9 @@
+import tiktoken
+
+encoding  = tiktoken.encoding_for_model("gpt-4.1-mini")
+
+tokens = encoding.encode("Hi, my name is Ed and I like banoffee pie")
+
+for token_id in tokens:
+    token_text = encoding.decode([token_id])
+    print(f"{token_id} = {token_text}")
